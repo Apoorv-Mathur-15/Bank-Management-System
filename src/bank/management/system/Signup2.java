cef55a8e78af3d5606e58e4a24136fa6b7ef756f
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-04-14 21:11:25 +0530

    14th April 2025

--- src/bank/management/system/Signup2.java
+++ src/bank/management/system/Signup2.java
@@ -6,6 +6,8 @@ import java.awt.*;
 public class Signup2 extends JFrame {
     String formNo;
 
+    JComboBox comboBox;
+
     Signup2(String first){
         super("Application Form");
 
@@ -17,9 +19,33 @@ public class Signup2 extends JFrame {
         add(bankImage);
 
         this.formNo = formNo;
+
+        JLabel label1 = new JLabel("Page 2:");
+        label1.setFont(new Font("Raleway", Font.BOLD, 22));
+        label1.setBounds(300, 0, 600, 40);
+        add(label1);
+
+        JLabel label2 = new JLabel("Additional Details");
+        label2.setFont(new Font("Raleway", Font.BOLD, 22));
+        label2.setBounds(300, 60, 600, 40);
+        add(label2);
+
+        JLabel label3 = new JLabel("Religion");
+        label3.setFont(new Font("Raleway", Font.BOLD, 18));
+        label3.setBounds(100, 120, 100, 30);
+        add(label3);
+
+        String[] religion = {"Hindu", "Muslim", "Sikh", "Christian", "Other"};
+        comboBox = new JComboBox<>(religion);
+        comboBox.setBackground(new Color(252, 208, 076));
+        comboBox.setFont(new Font("Raleway", Font.BOLD, 18));
+        comboBox.setBounds(350, 120, 320, 30);
+        add(comboBox);
+
+
         setSize(850, 750);
         setLocation(450, 80);
-        getContentPane().setBackground(new Color(2, 250, 155));
+        getContentPane().setBackground(new Color(252, 208, 076));
         setLayout(null);
         setVisible(true);
     }
